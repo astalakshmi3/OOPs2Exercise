1b28427a8d82b5c66e67c6184433f99dfbd5a055
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2025-12-30 09:42:47 +0100

    oops2 Exercise Booking

--- src/main/java/astalakshmi/example/Main.java
+++ src/main/java/astalakshmi/example/Main.java
@@ -1,4 +1,16 @@
 package astalakshmi.example;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Booking System Initialized");
+        BookingRequest request = new BookingRequest("Alice", "2:00 PM", true, 0);
+       // Switch between Member and Non-Member Booking Processor
+        BookingProcessor processor= new BookingProcessor.MemberBookingProcessor();
+        // new non-member processor
+        // BookingProcessor processor= new BookingProcessor.NonMemberBookingProcessor();
+        Booking booking = processor.processBooking(request);
+        System.out.println("Booking Reference: " + booking.getReferenceNumber());
+
+    }
+
 }
